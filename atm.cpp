5409ced31de01612653b76bce2d49abf4d2490fb
RahmatRamadhan17 <rahmat.ramadhan0217@gmail.com>
2025-12-04 14:48:32 +0700

    Coba2

--- atm.cpp
+++ atm.cpp
@@ -1,1 +1,34 @@
-#include<iostream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+// ===================
+// Global Data
+// ===================
+int saldo = 100000;
+string riwayat[20];
+int indexRiwayat = 0;
+
+
+// ===================
+// Fungsi Riwayat
+// ===================
+void tambahRiwayat(string msg) {
+    if (indexRiwayat < 20) {
+        riwayat[indexRiwayat] = msg;
+        indexRiwayat++;
+    }
+}
+
+void tampilRiwayat() {
+    cout << "\n===== RIWAYAT TRANSAKSI =====\n";
+
+    if (indexRiwayat == 0) {
+        cout << "Belum ada transaksi.\n";
+        return;
+    }
+
+    for (int i = 0; i < indexRiwayat; i++) {
+        cout << i + 1 << ". " << riwayat[i] << endl;
+    }
+}
